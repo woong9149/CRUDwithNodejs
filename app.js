120f88cd1a5c3b89ccd4560be0c2b0c1794ef6ed
“웅이” <sewoong0409@naver.com>
2020-04-15 18:38:31 +0900

    git test

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    
+
     if(_url == '/'){
         _url = '/index.html';
     }
