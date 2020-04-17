7ea0381c7151b65b23c09d0e418823d08759bba9
“웅이” <sewoong0409@naver.com>
2020-04-17 09:59:05 +0900

    post방식으로 데이터전송

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
 function templateHTML(title,list,body){
     return `
@@ -13,6 +14,7 @@ function templateHTML(title,list,body){
         <body>
             <h1><a href="/">WEB</a></h1>
             ${list}
+            <a href="/create">create</a>
             ${body}
         </body>
         </html>
@@ -53,13 +55,47 @@ var app = http.createServer(function(request,response){
                 var title = queryData.id;
                 var list = templateList(filelist);
                 var template = templateHTML(title,list,`<h2>${title}</h2>${description}`);
-            response.writeHead(200);
-            response.end(template);
+                response.writeHead(200);
+                response.end(template);
+                })
             })
-        })
         }
             
-    }else{
+    }else if(pathname === '/create'){
+        fs.readdir('./data',function(error,filelist){
+            fs.readFile(`data/${queryData.id}`,'utf8',function(err,description){
+                var title = queryData.id;
+                var list = templateList(filelist);
+                var template = templateHTML(title,list,`
+                    <form action="/create_process" method="post">
+                        <p><input type="text" name="title" placeholder="title"></p>
+                        <p>
+                            <textarea name="description" placeholder="description"></textarea>
+                        </p>
+                        <p>
+                            <input type="submit">
+                        </p>
+                    </form>
+                `);
+                response.writeHead(200);
+                response.end(template);
+                })
+            })
+    }else if(pathname === '/create_process'){
+        var body = '';
+        request.on('data', function(data){
+            body = body + data;
+        });
+        request.on('end',function(){
+            var post = qs.parse(body);
+            var title = post.title;
+            var description = post.description;
+            fs.writeFile(`data/${title}`,description,'utf8',function(err){
+                response.writeHead(302,{Location: `/?id=${title}`});
+                response.end('success !');
+            })
+        });
+    } else{
         response.writeHead(404);
         response.end('Not Found');
     }
