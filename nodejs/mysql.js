724fe46c8f5bb6deb5c244e10050d62df6989fde
“웅이” <sewoong0409@naver.com>
2020-04-21 23:50:48 +0900

    fs -> mysql 변환

---
+++ nodejs/mysql.js
@@ -0,0 +1,24 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'boa457813',
+    database:'opentutorials'
+});
+
+connection.connect();
+
+connection.query('SELECT * FROM topic',function(error,results,fields){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+});
+
+connection.end();
+
+
+
+
+
