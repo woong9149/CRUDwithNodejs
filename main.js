d3304a8269f93fd84ac7052a460248ee016da26b
“웅이” <sewoong0409@naver.com>
2020-04-28 23:53:27 +0900

    querystring ->cleanURL로 변경

--- main.js
+++ main.js
@@ -4,22 +4,63 @@ const url = require('url')
 const topic = require('./lib/topic');
 const author = require('./lib/author');
 
+//body : 웹브라우저쪽에서 요청한 정보의 본체를 body라함.
+//body-parser: 그 body를 분석해서 사용함.
 
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/',(req,res) => {
     var _url = req.url;
     var queryData = url.parse(_url,true).query;
-    if(queryData.id === undefined){
-        topic.home(req,res);
-    }else{
-        topic.page(req,res);
-    }
+    topic.home(req,res);
+    // if(queryData.id === undefined){
+    //     topic.home(req,res);
+    // }else{
+    //     topic.page(req,res);
+    // }
  
 })
 
-app.get('/page/:pageId/:chapterId',(req,res)=>{
-    res.send(req.params);
+app.get('/page/:pageId',(req,res)=>{
+    topic.page(req,res);
 })
+
+app.get('/create',(req,res)=>{
+    topic.create(req,res);
+})
+
+app.post('/create_process',(req,res)=>{
+    topic.create_process(req,res);
+})
+
+app.get('/update/:updateId',(req,res)=>{
+    topic.update(req,res);
+})
+
+app.post('/update_process',(req,res)=>{
+    topic.update_process(req,res);
+})
+
+app.post('/delete_process',(req,res)=>{
+    topic.delete_process(req,res);
+})
+
+app.get('/authorTable',(req,res)=>{
+    author.home(req,res);
+})
+
+app.post('/author_create_process',(req,res)=>{
+    author.create_process(req,res);
+})
+
+app.get('/author_update/:author_updateId',(req,res)=>{
+    author.update(req,res);
+})
+
+app.post('/author_update_process', (req,res)=>{
+    author.update_process(req,res);
+})
+
 app.listen(3000, ()=> console.log('Example app listening on port 3000!'))
 
 /*
