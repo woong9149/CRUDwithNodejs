89b2c5ee33be21968aefa39c8aa09dc4156b0852
“웅이” <sewoong0409@naver.com>
2020-05-04 23:54:19 +0900

    response HEADER 쿠키적용

---
+++ nodejs/cookie.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var cookie = require('cookie');
+
+http.createServer(function(request,response){
+    
+    var cookies = {};
+    if(request.headers.cookie !== undefined){
+        cookies = cookie.parse(request.headers.cookie);
+    }
+    console.log('yummy_cookie: ',cookies.yummy_cookie);
+    response.writeHead(200,{
+        'Set-Cookie':['yummy_cookie=choco','tasty_cookie=strawberry']
+    });
+    response.end('Cookie');
+}).listen(3000);
+
+
+
+
+
+
--- practice.js
+++ practice.js
@@ -1,4 +1,22 @@
+DB_BACKUP="/home/dbbackup"
 
+DB_USER="taxfriends"
+
+DB_PASSWD="tpclsrnelql!@"
+
+db="taxfriend2"
+
+table="tablename"
+
+
+
+# Remove backups older than 1 days
+
+find $DB_BACKUP -ctime +1 -exec rm -f {} \;
+
+
+# 데이터베이스를 모두 백업할경우
+mysqldump --user=$DB_USER --password=$DB_PASSWD -A | gzip > "$DB_BACKUP/mysqldump-$db-$(date +%Y-%m-%d).gz";
 
 
 
